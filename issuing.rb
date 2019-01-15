ba4f158baebe3fb9e4581f20c3a9a7814b8a610f
Annie Martens <anniecmartens@gmail.com>
2019-01-15 16:51:16 -0600

    get_conversations

--- issuing.rb
+++ issuing.rb
@@ -1,7 +1,26 @@
 require 'sinatra'
 require 'dotenv'
 require 'json'
+require 'intercom'
 
 get '/' do 
 	erb :index
+end
+
+post '/' do 
+	@id = params[:id]
+	initialize_intercom
+	get_conversations
+	erb :issues
+end
+
+def initialize_intercom
+	if @intercom.nil? 
+		token = ENV['token']
+		@intercom = Intercom::Client.new(token: token)
+	end
+end
+
+def get_conversations
+	@conversations = @intercom.conversations.find_all(:type => 'user', :intercom_user_id => @id)
 end
