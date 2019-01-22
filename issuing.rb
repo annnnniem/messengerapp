f74220824463207e31a9249434195f3307da07aa
Annie Martens <anniecmartens@gmail.com>
2019-01-22 15:18:27 -0600

    works for all gh notes

--- issuing.rb
+++ issuing.rb
@@ -15,22 +15,7 @@ post '/' do
 	initialize_intercom
 	@conversations = @intercom.conversations.find_all(:type => 'user', :intercom_user_id => @id)
 	@conversations.each do |i|
-		#get each convo
-		convo = @intercom.conversations.find(:id => i.id, :display_as => 'plaintext')
-		#get parts on the gotten convo
-		convo_parts = convo.conversation_parts
-		#look in each convo part
-		convo_parts.each do |m|
-			if !m.body.nil? && m.part_type == "note"
-				downcased_body = m.body.downcase
-				#for some reason, only pasted in links from gh work, not the bot comment. potentially because not plaintext?
-				if downcased_body.include? "github"
-					@github_convos.store(i.id, m.body)
-				else
-					break
-				end
-			end
-		end
+		get_conversation(i)
 	end
 	puts @github_convos
 	erb :issues
@@ -41,5 +26,23 @@ def initialize_intercom
 	if @intercom.nil? then
 		token = ENV['token']
 		@intercom = Intercom::Client.new(token: token)
+	end
+end
+
+def get_conversation(conversation)
+	#get each convo
+	@convo = @intercom.conversations.find(:id => conversation.id)
+	#get parts on the gotten convo
+	convo_parts = @convo.conversation_parts
+	#look in each convo part
+	convo_parts.each do |m|
+		if !m.body.nil? && m.part_type == "note"
+			downcased_body = m.body.downcase
+			if downcased_body.include? "github"
+				@github_convos.store(conversation.id, m.body)
+			else
+				break
+			end
+		end
 	end
 end
