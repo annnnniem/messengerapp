e2afd6824aa7d4004d79433ec9301b61d7307ed6
Annie Martens <anniecmartens@gmail.com>
2019-01-22 14:59:16 -0600

    only gh links pasted into convo

--- issuing.rb
+++ issuing.rb
@@ -9,22 +9,23 @@ end
 
 post '/' do 
 	#array to hold the convos
-	@github_convos = []
+	@github_convos = {}
 	#get the user id from the field input
 	@id = params[:id]
 	initialize_intercom
 	@conversations = @intercom.conversations.find_all(:type => 'user', :intercom_user_id => @id)
 	@conversations.each do |i|
 		#get each convo
-		convo = @intercom.conversations.find(:id => i.id)
+		convo = @intercom.conversations.find(:id => i.id, :display_as => 'plaintext')
 		#get parts on the gotten convo
 		convo_parts = convo.conversation_parts
 		#look in each convo part
 		convo_parts.each do |m|
-			if !m.body.nil? 
-				if m.body.include? "github"
-					puts m.body
-					@github_convos << m.body
+			if !m.body.nil? && m.part_type == "note"
+				downcased_body = m.body.downcase
+				#for some reason, only pasted in links from gh work, not the bot comment. potentially because not plaintext?
+				if downcased_body.include? "github"
+					@github_convos.store(i.id, m.body)
 				else
 					break
 				end
